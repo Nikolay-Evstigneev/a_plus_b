41ea1e65dea479c6351c95c6d2201f96db6f8350
Nikolay-Evstigneev <71175033+Nikolay-Evstigneev@users.noreply.github.com>
2020-09-12 16:24:41 +0500

    a_plus_b.cpp

---
+++ a_plus_b.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int a, b, c;
+int main() {
+cout <<"Введите числа а и в" <<;
+cin >> a;
+cin >> b;
+c = a + b;
+cout <<"сумма чисел:" << c;
+getch();
+return 0;
+}
